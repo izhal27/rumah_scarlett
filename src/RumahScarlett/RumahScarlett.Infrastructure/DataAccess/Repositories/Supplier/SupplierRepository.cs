81af826919e513d6515dd41382e95f0776ad4924
Risal Walangadi <risal.walangadi@gmail.com>
2019-08-27 09:41:14 +0800

    supplier repository

--- src/RumahScarlett/RumahScarlett.Infrastructure/DataAccess/Repositories/Supplier/SupplierRepository.cs
+++ src/RumahScarlett/RumahScarlett.Infrastructure/DataAccess/Repositories/Supplier/SupplierRepository.cs
@@ -1,6 +1,5 @@
 ﻿using Dapper;
 using Dapper.Contrib.Extensions;
-using MySql.Data.MySqlClient;
 using RumahScarlett.CommonComponents;
 using RumahScarlett.Domain.Models.Supplier;
 using RumahScarlett.Services.Services.Supplier;
@@ -14,60 +13,71 @@ namespace RumahScarlett.Infrastructure.DataAccess.Repositories.Supplier
 {
    public class SupplierRepository : BaseRepository<ISupplierModel>, ISupplierRepository
    {
-      private DbContext _context;
-
       public SupplierRepository()
       {
-         _context = new DbContext();
          _modelName = "supplier";
       }
 
       public void Insert(ISupplierModel model)
       {
          var dataAccessStatus = new DataAccessStatus();
-         ValidateModel(model, dataAccessStatus);
 
-         Insert(model, () => _context.Conn.Insert((SupplierModel)model), dataAccessStatus,
-                () => CheckInsert(model));
+         using (var context = new DbContext())
+         {
+            ValidateModel(context, model, dataAccessStatus);
+
+            Insert(model, () => context.Conn.Insert((SupplierModel)model), dataAccessStatus,
+                   () => CheckInsert(context, model));
+         }
       }
 
       public void Update(ISupplierModel model)
       {
          var dataAccessStatus = new DataAccessStatus();
-         ValidateModel(model, dataAccessStatus);
 
-         Update(model, () => _context.Conn.Update((SupplierModel)model), dataAccessStatus,
-                () => CheckUpdateDelete(model));
+         using (var context = new DbContext())
+         {
+            ValidateModel(context, model, dataAccessStatus);
+
+            Update(model, () => context.Conn.Update((SupplierModel)model), dataAccessStatus,
+                   () => CheckUpdateDelete(context, model));
+         }
       }
 
       public void Delete(ISupplierModel model)
       {
          var dataAccessStatus = new DataAccessStatus();
 
-         Delete(model, () => _context.Conn.Delete((SupplierModel)model), dataAccessStatus,
-                () => CheckUpdateDelete(model));
+         using (var context = new DbContext())
+         {
+            Delete(model, () => context.Conn.Delete((SupplierModel)model), dataAccessStatus,
+                () => CheckUpdateDelete(context, model));
+         }
       }
 
       public IEnumerable<ISupplierModel> GetAll()
       {
          var dataAccessStatus = new DataAccessStatus();
 
-         return GetAll(() =>
+         using (var context = new DbContext())
          {
-            return _context.Conn.GetAll<SupplierModel>().ToList();
-         }, dataAccessStatus);
+            return GetAll(() => { return context.Conn.GetAll<SupplierModel>().ToList(); }, dataAccessStatus);
+         }
       }
 
       public ISupplierModel GetById(object id)
       {
          var dataAccessStatus = new DataAccessStatus();
 
-         return GetBy(() => _context.Conn.Get<SupplierModel>(id), dataAccessStatus);
+         using (var context = new DbContext())
+         {
+            return GetBy(() => context.Conn.Get<SupplierModel>(id), dataAccessStatus);
+         }
       }
 
-      private void ValidateModel(ISupplierModel model, DataAccessStatus dataAccessStatus)
+      private void ValidateModel(DbContext context, ISupplierModel model, DataAccessStatus dataAccessStatus)
       {
-         var existsNama = _context.Conn.ExecuteScalar<bool>("SELECT COUNT(1) FROM supplier WHERE nama=@nama AND id!=@id",
+         var existsNama = context.Conn.ExecuteScalar<bool>("SELECT COUNT(1) FROM supplier WHERE nama=@nama AND id!=@id",
                                                              new { model.nama, model.id });
 
          if (existsNama)
@@ -79,16 +89,16 @@ namespace RumahScarlett.Infrastructure.DataAccess.Repositories.Supplier
          }
       }
 
-      private bool CheckInsert(ISupplierModel model)
+      private bool CheckInsert(DbContext context, ISupplierModel model)
       {
-         return _context.Conn.ExecuteScalar<bool>("SELECT COUNT(1) FROM supplier WHERE nama=@nama "
+         return context.Conn.ExecuteScalar<bool>("SELECT COUNT(1) FROM supplier WHERE nama=@nama "
                                                   + "AND id=(SELECT LAST_INSERT_ID())",
                                                   new { model.nama });
       }
 
-      private bool CheckUpdateDelete(ISupplierModel model)
+      private bool CheckUpdateDelete(DbContext context, ISupplierModel model)
       {
-         return _context.Conn.ExecuteScalar<bool>("SELECT COUNT(1) FROM supplier WHERE id=@id",
+         return context.Conn.ExecuteScalar<bool>("SELECT COUNT(1) FROM supplier WHERE id=@id",
                                                   new { model.id });
       }
    }
