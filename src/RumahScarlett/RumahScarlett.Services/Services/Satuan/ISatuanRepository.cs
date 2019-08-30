e9ea84ee7580486fe88cbbbe4e76814c1dfa80e5
Risal Walangadi <risal.walangadi@gmail.com>
2019-08-30 09:07:14 +0800

    add satuan services

---
+++ src/RumahScarlett/RumahScarlett.Services/Services/Satuan/ISatuanRepository.cs
@@ -0,0 +1,13 @@
+﻿using RumahScarlett.Domain.Models.Satuan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Services.Services.Satuan
+{
+   public interface ISatuanRepository : IBaseRepository<ISatuanModel>
+   {
+   }
+}
---
+++ src/RumahScarlett/RumahScarlett.Services/Services/Satuan/SatuanServices.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RumahScarlett.Domain.Models.Satuan;
+
+namespace RumahScarlett.Services.Services.Satuan
+{
+   public class SatuanServices : ISatuanServices
+   {
+      private IModelDataAnnotationCheck _modelDAC;
+      private ISatuanRepository _repo;
+
+      public SatuanServices(ISatuanRepository repo, IModelDataAnnotationCheck modelDAC)
+      {
+         _repo = repo;
+         _modelDAC = modelDAC;
+      }
+
+      public void Insert(ISatuanModel model)
+      {
+         ValidateModel(model);
+         _repo.Insert(model);
+      }
+
+      public void Update(ISatuanModel model)
+      {
+         ValidateModel(model);
+         _repo.Update(model);
+      }
+
+      public void Delete(ISatuanModel model)
+      {
+         _repo.Delete(model);
+      }
+
+      public IEnumerable<ISatuanModel> GetAll()
+      {
+         return _repo.GetAll();
+      }
+
+      public ISatuanModel GetById(object id)
+      {
+         return _repo.GetById(id);
+      }
+
+      public void ValidateModel(ISatuanModel model)
+      {
+         _modelDAC.ValidateModel(model);
+      }
+   }
+}
