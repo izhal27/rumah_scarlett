f8cabc170b5c5a8c24dac0e6f39634c5d31b2ed2
Risal Walangadi <risal.walangadi@gmail.com>
2019-08-30 09:00:11 +0800

    add satuan model

---
+++ src/RumahScarlett/RumahScarlett.Domain/Models/Satuan/ISatuanModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RumahScarlett.Domain.Models.Satuan
+{
+   public interface ISatuanModel
+   {
+      uint id { get; set; }
+      string nama { get; set; }
+      string keterangan { get; set; }
+   }
+}
---
+++ src/RumahScarlett/RumahScarlett.Domain/Models/Satuan/SatuanModel.cs
@@ -0,0 +1,30 @@
+﻿using Dp = Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Domain.Models.Satuan
+{
+   [Table("satuan")]
+   public class SatuanModel : ISatuanModel
+   {
+      [Browsable(false)]
+      [Display(Name = "ID")]
+      public uint id { get; set; }
+
+      [Required(AllowEmptyStrings = false, ErrorMessage = "Nama satuan harus diisi !!!")]
+      [StringLength(100, MinimumLength = 3, ErrorMessage = "Nama satuan harus diantara 3 sampai 100 karakter !!!")]
+      [Display(Name = "Nama")]
+      public string nama { get; set; }
+
+      [DefaultValue("")]
+      [StringLength(255, ErrorMessage = "Panjang maksimal keterangan 255 karakter !!!")]
+      [Display(Name = "Keterangan")]
+      public string keterangan { get; set; }
+   }
+}
