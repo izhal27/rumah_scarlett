3dcc1c5b24bb7de88e6c522c06655b95aec2f529
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-01 01:05:57 +0800

    add hutang operasional service and repository interface

---
+++ src/RumahScarlett/RumahScarlett.Services/Services/HutangOperasional/HutangOperasionalServices.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RumahScarlett.Domain.Models.HutangOperasional;
+
+namespace RumahScarlett.Services.Services.HutangOperasional
+{
+   public class HutangOperasionalServices : IHutangOperasionalServices
+   {
+      private IHutangOperasionalRepository _repo;
+      private IModelDataAnnotationCheck _modelDAC;
+
+      public HutangOperasionalServices(IHutangOperasionalRepository repo, IModelDataAnnotationCheck modelDAC)
+      {
+         _repo = repo;
+         _modelDAC = modelDAC;
+      }
+
+      public void Insert(IHutangOperasionalModel model)
+      {
+         ValidateModel(model);
+         _repo.Insert(model);
+      }
+
+      public void Update(IHutangOperasionalModel model)
+      {
+         ValidateModel(model);
+         _repo.Update(model);
+      }
+
+      public void Delete(IHutangOperasionalModel model)
+      {
+         _repo.Delete(model);
+      }
+
+      public IEnumerable<IHutangOperasionalModel> GetAll()
+      {
+         return _repo.GetAll();
+      }
+
+      public IHutangOperasionalModel GetById(object id)
+      {
+         return _repo.GetById(id);
+      }
+
+      public void ValidateModel(IHutangOperasionalModel model)
+      {
+         _modelDAC.ValidateModel(model);
+      }
+   }
+}
---
+++ src/RumahScarlett/RumahScarlett.Services/Services/HutangOperasional/IHutangOperasionalRepository.cs
@@ -0,0 +1,13 @@
+﻿using RumahScarlett.Domain.Models.HutangOperasional;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Services.Services.HutangOperasional
+{
+   public interface IHutangOperasionalRepository : IBaseRepository<IHutangOperasionalModel>
+   {
+   }
+}
