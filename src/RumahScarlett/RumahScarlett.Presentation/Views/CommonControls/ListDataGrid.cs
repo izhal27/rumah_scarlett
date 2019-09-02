9221dd9dbc5d0ecb1e77ef3cc16396894ecec370
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-02 11:07:32 +0800

    improve list data grid

--- src/RumahScarlett/RumahScarlett.Presentation/Views/CommonControls/ListDataGrid.cs
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/CommonControls/ListDataGrid.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace RumahScarlett.Presentation.Views.CommonControls
 {
@@ -15,12 +16,28 @@ namespace RumahScarlett.Presentation.Views.CommonControls
    {
       public ListDataGrid()
       {
+         ShowRowHeader = true;
          AllowEditing = false;
          AutoSizeColumnsMode = AutoSizeColumnsMode.Fill;
          Anchor = AnchorStyles.Top | AnchorStyles.Right |
                   AnchorStyles.Bottom | AnchorStyles.Left;
 
+         DrawCell += ListDataGrid_DrawCell;
          QueryRowStyle += ListDataGrid_QueryRowStyle;
+      }
+
+      private void ListDataGrid_DrawCell(object sender, DrawCellEventArgs e)
+      {
+         if (ShowRowHeader && e != null && e.RowIndex != 0)
+         {
+            if (e.ColumnIndex == 0)
+            {
+               e.DisplayText = e.RowIndex.ToString();
+               e.Style.TextColor = Color.Black;
+               e.Style.VerticalAlignment = VerticalAlignment.Center;
+               e.Style.HorizontalAlignment = HorizontalAlignment.Center;
+            }
+         }
       }
 
       private void ListDataGrid_QueryRowStyle(object sender, QueryRowStyleEventArgs e)
