34711577b16fd374aac36ff0d18b3a41af854d4a
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-06 16:28:17 +0800

    add penyesuaian stok entry view

---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/PenyesuaianStok/PenyesuaianStokEntryView.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace RumahScarlett.Presentation.Views.PenyesuaianStok
+{
+   partial class PenyesuaianStokEntryView
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+         this.textBoxKeterangan = new RumahScarlett.Presentation.Views.CommonControls.BaseTextBox();
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.dateTimePickerTanggal = new System.Windows.Forms.DateTimePicker();
+         this.textBoxDigitJumlah = new RumahScarlett.Presentation.Views.CommonControls.BaseTextBoxDigit();
+         this.label8 = new System.Windows.Forms.Label();
+         this.label5 = new System.Windows.Forms.Label();
+         this.label6 = new System.Windows.Forms.Label();
+         this.label7 = new System.Windows.Forms.Label();
+         this.textBoxNamaBarang = new RumahScarlett.Presentation.Views.CommonControls.BaseTextBox();
+         this.buttonCari = new System.Windows.Forms.Button();
+         this.label4 = new System.Windows.Forms.Label();
+         this.label9 = new System.Windows.Forms.Label();
+         this.label10 = new System.Windows.Forms.Label();
+         this.label11 = new System.Windows.Forms.Label();
+         this.tableLayoutPanel1.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.textBoxKeterangan)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.textBoxDigitJumlah)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.textBoxNamaBarang)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // panelUp
+         // 
+         this.panelUp.Size = new System.Drawing.Size(391, 40);
+         // 
+         // operationButtons
+         // 
+         this.operationButtons.Location = new System.Drawing.Point(228, 255);
+         // 
+         // tableLayoutPanel1
+         // 
+         this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.tableLayoutPanel1.ColumnCount = 5;
+         this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+         this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+         this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+         this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+         this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+         this.tableLayoutPanel1.Controls.Add(this.textBoxKeterangan, 2, 3);
+         this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+         this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+         this.tableLayoutPanel1.Controls.Add(this.label3, 0, 3);
+         this.tableLayoutPanel1.Controls.Add(this.dateTimePickerTanggal, 2, 0);
+         this.tableLayoutPanel1.Controls.Add(this.textBoxDigitJumlah, 2, 2);
+         this.tableLayoutPanel1.Controls.Add(this.label8, 4, 2);
+         this.tableLayoutPanel1.Controls.Add(this.label5, 4, 0);
+         this.tableLayoutPanel1.Controls.Add(this.label6, 4, 3);
+         this.tableLayoutPanel1.Controls.Add(this.label7, 0, 1);
+         this.tableLayoutPanel1.Controls.Add(this.textBoxNamaBarang, 2, 1);
+         this.tableLayoutPanel1.Controls.Add(this.buttonCari, 3, 1);
+         this.tableLayoutPanel1.Controls.Add(this.label4, 1, 0);
+         this.tableLayoutPanel1.Controls.Add(this.label9, 1, 1);
+         this.tableLayoutPanel1.Controls.Add(this.label10, 1, 2);
+         this.tableLayoutPanel1.Controls.Add(this.label11, 1, 3);
+         this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 58);
+         this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+         this.tableLayoutPanel1.RowCount = 4;
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+         this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+         this.tableLayoutPanel1.Size = new System.Drawing.Size(391, 191);
+         this.tableLayoutPanel1.TabIndex = 4;
+         // 
+         // textBoxKeterangan
+         // 
+         this.textBoxKeterangan.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.textBoxKeterangan.BeforeTouchSize = new System.Drawing.Size(250, 20);
+         this.textBoxKeterangan.Location = new System.Drawing.Point(87, 86);
+         this.textBoxKeterangan.Metrocolor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+         this.textBoxKeterangan.Multiline = true;
+         this.textBoxKeterangan.Name = "textBoxKeterangan";
+         this.textBoxKeterangan.Size = new System.Drawing.Size(250, 100);
+         this.textBoxKeterangan.Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Default;
+         this.textBoxKeterangan.TabIndex = 3;
+         // 
+         // label1
+         // 
+         this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(3, 6);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(46, 13);
+         this.label1.TabIndex = 0;
+         this.label1.Text = "Tanggal";
+         // 
+         // label2
+         // 
+         this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(3, 61);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(23, 13);
+         this.label2.TabIndex = 0;
+         this.label2.Text = "Qty";
+         // 
+         // label3
+         // 
+         this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label3.AutoSize = true;
+         this.label3.Location = new System.Drawing.Point(3, 129);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(62, 13);
+         this.label3.TabIndex = 0;
+         this.label3.Text = "Keterangan";
+         // 
+         // dateTimePickerTanggal
+         // 
+         this.dateTimePickerTanggal.Location = new System.Drawing.Point(87, 3);
+         this.dateTimePickerTanggal.MinDate = new System.DateTime(1945, 8, 17, 0, 0, 0, 0);
+         this.dateTimePickerTanggal.Name = "dateTimePickerTanggal";
+         this.dateTimePickerTanggal.Size = new System.Drawing.Size(150, 20);
+         this.dateTimePickerTanggal.TabIndex = 1;
+         // 
+         // textBoxDigitJumlah
+         // 
+         this.textBoxDigitJumlah.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.textBoxDigitJumlah.BackGroundColor = System.Drawing.SystemColors.Window;
+         this.textBoxDigitJumlah.BeforeTouchSize = new System.Drawing.Size(250, 20);
+         this.textBoxDigitJumlah.IntegerValue = ((long)(0));
+         this.textBoxDigitJumlah.Location = new System.Drawing.Point(87, 58);
+         this.textBoxDigitJumlah.Metrocolor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+         this.textBoxDigitJumlah.MinValue = ((long)(0));
+         this.textBoxDigitJumlah.Name = "textBoxDigitJumlah";
+         this.textBoxDigitJumlah.NullString = "";
+         this.textBoxDigitJumlah.Size = new System.Drawing.Size(150, 20);
+         this.textBoxDigitJumlah.Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Default;
+         this.textBoxDigitJumlah.TabIndex = 2;
+         this.textBoxDigitJumlah.Text = "0";
+         this.textBoxDigitJumlah.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+         // 
+         // label8
+         // 
+         this.label8.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label8.AutoSize = true;
+         this.label8.ForeColor = System.Drawing.Color.Red;
+         this.label8.Location = new System.Drawing.Point(372, 61);
+         this.label8.Name = "label8";
+         this.label8.Size = new System.Drawing.Size(11, 13);
+         this.label8.TabIndex = 5;
+         this.label8.Text = "*";
+         // 
+         // label5
+         // 
+         this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label5.AutoSize = true;
+         this.label5.ForeColor = System.Drawing.Color.Red;
+         this.label5.Location = new System.Drawing.Point(372, 6);
+         this.label5.Name = "label5";
+         this.label5.Size = new System.Drawing.Size(11, 13);
+         this.label5.TabIndex = 5;
+         this.label5.Text = "*";
+         // 
+         // label6
+         // 
+         this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label6.AutoSize = true;
+         this.label6.ForeColor = System.Drawing.Color.Red;
+         this.label6.Location = new System.Drawing.Point(372, 129);
+         this.label6.Name = "label6";
+         this.label6.Size = new System.Drawing.Size(11, 13);
+         this.label6.TabIndex = 5;
+         this.label6.Text = "*";
+         // 
+         // label7
+         // 
+         this.label7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label7.AutoSize = true;
+         this.label7.Location = new System.Drawing.Point(3, 34);
+         this.label7.Name = "label7";
+         this.label7.Size = new System.Drawing.Size(41, 13);
+         this.label7.TabIndex = 0;
+         this.label7.Text = "Barang";
+         // 
+         // textBoxNamaBarang
+         // 
+         this.textBoxNamaBarang.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.textBoxNamaBarang.BeforeTouchSize = new System.Drawing.Size(250, 20);
+         this.textBoxNamaBarang.Location = new System.Drawing.Point(87, 30);
+         this.textBoxNamaBarang.Metrocolor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+         this.textBoxNamaBarang.Name = "textBoxNamaBarang";
+         this.textBoxNamaBarang.ReadOnly = true;
+         this.textBoxNamaBarang.Size = new System.Drawing.Size(250, 20);
+         this.textBoxNamaBarang.Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Default;
+         this.textBoxNamaBarang.TabIndex = 6;
+         // 
+         // buttonCari
+         // 
+         this.buttonCari.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.buttonCari.Location = new System.Drawing.Point(343, 29);
+         this.buttonCari.Name = "buttonCari";
+         this.buttonCari.Size = new System.Drawing.Size(23, 23);
+         this.buttonCari.TabIndex = 7;
+         this.buttonCari.Text = "C";
+         this.buttonCari.UseVisualStyleBackColor = true;
+         // 
+         // label4
+         // 
+         this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label4.AutoSize = true;
+         this.label4.Location = new System.Drawing.Point(71, 6);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(10, 13);
+         this.label4.TabIndex = 0;
+         this.label4.Text = ":";
+         // 
+         // label9
+         // 
+         this.label9.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label9.AutoSize = true;
+         this.label9.Location = new System.Drawing.Point(71, 34);
+         this.label9.Name = "label9";
+         this.label9.Size = new System.Drawing.Size(10, 13);
+         this.label9.TabIndex = 0;
+         this.label9.Text = ":";
+         // 
+         // label10
+         // 
+         this.label10.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label10.AutoSize = true;
+         this.label10.Location = new System.Drawing.Point(71, 61);
+         this.label10.Name = "label10";
+         this.label10.Size = new System.Drawing.Size(10, 13);
+         this.label10.TabIndex = 0;
+         this.label10.Text = ":";
+         // 
+         // label11
+         // 
+         this.label11.Anchor = System.Windows.Forms.AnchorStyles.Left;
+         this.label11.AutoSize = true;
+         this.label11.Location = new System.Drawing.Point(71, 129);
+         this.label11.Name = "label11";
+         this.label11.Size = new System.Drawing.Size(10, 13);
+         this.label11.TabIndex = 0;
+         this.label11.Text = ":";
+         // 
+         // PenyesuaianStokEntryView
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(415, 302);
+         this.Controls.Add(this.tableLayoutPanel1);
+         this.Name = "PenyesuaianStokEntryView";
+         this.Text = "Penyesuaian Stok";
+         this.Controls.SetChildIndex(this.panelUp, 0);
+         this.Controls.SetChildIndex(this.operationButtons, 0);
+         this.Controls.SetChildIndex(this.tableLayoutPanel1, 0);
+         this.tableLayoutPanel1.ResumeLayout(false);
+         this.tableLayoutPanel1.PerformLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.textBoxKeterangan)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.textBoxDigitJumlah)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.textBoxNamaBarang)).EndInit();
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+      private CommonControls.BaseTextBox textBoxKeterangan;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.Label label3;
+      private System.Windows.Forms.DateTimePicker dateTimePickerTanggal;
+      private CommonControls.BaseTextBoxDigit textBoxDigitJumlah;
+      private System.Windows.Forms.Label label8;
+      private System.Windows.Forms.Label label5;
+      private System.Windows.Forms.Label label6;
+      private System.Windows.Forms.Label label7;
+      private CommonControls.BaseTextBox textBoxNamaBarang;
+      private System.Windows.Forms.Button buttonCari;
+      private System.Windows.Forms.Label label4;
+      private System.Windows.Forms.Label label9;
+      private System.Windows.Forms.Label label10;
+      private System.Windows.Forms.Label label11;
+   }
+}
