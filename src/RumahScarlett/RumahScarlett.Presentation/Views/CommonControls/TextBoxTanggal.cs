830f849c8876573d09bdf50b85648fee1143d5e6
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-07 10:44:16 +0800

    add common controls textbox tanggal

---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/CommonControls/TextBoxTanggal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Presentation.Views.CommonControls
+{
+   public class TextBoxTanggal : BaseTextBox
+   {
+      protected override void OnCreateControl()
+      {
+         Text = DateTime.Now.Date.ToString("dd/MM/yyyy");
+      }
+   }
+}
