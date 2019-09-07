69e8490a6defc3e2136ee6a240e9ab1e49f64544
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-07 09:47:34 +0800

    add interface transaksi view

---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/ITransaksiView.cs
@@ -0,0 +1,26 @@
+﻿using RumahScarlett.Presentation.Views.CommonControls;
+using Syncfusion.WinForms.DataGrid.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RumahScarlett.Presentation.Views
+{
+   public interface ITransaksiView : IView
+   {
+      event EventHandler OnLoadData;
+      event EventHandler OnCariData;
+      event EventHandler OnHapusData;
+      event EventHandler OnSimpanData;
+      event EventHandler OnBersihkanData;
+      event EventHandler<CurrentCellKeyEventArgs> OnListDataGridCurrentCellKeyDown;
+      event EventHandler<CurrentCellActivatedEventArgs> OnListDataGridCurrentCellActivated;
+      event EventHandler<CurrentCellEndEditEventArgs> OnListDataGridCurrentCellEndEdit;
+      event EventHandler<PreviewKeyDownEventArgs> OnListDataGridPreviewKeyDown;
+
+      ListDataGrid ListDataGrid { get; }
+   }
+}
--- src/RumahScarlett/RumahScarlett.Presentation/Views/Pembelian/IPembelianView.cs
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/Pembelian/IPembelianView.cs
@@ -1,6 +1,4 @@
-﻿using RumahScarlett.Presentation.Views.CommonControls;
-using RumahScarlett.Presentation.Views.ModelControls;
-using Syncfusion.WinForms.DataGrid.Events;
+﻿using RumahScarlett.Presentation.Views.ModelControls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,19 +8,8 @@ using System.Windows.Forms;
 
 namespace RumahScarlett.Presentation.Views.Pembelian
 {
-   public interface IPembelianView : IView
+   public interface IPembelianView : ITransaksiView
    {
-      event EventHandler OnLoadData;
-      event EventHandler OnCariData;
-      event EventHandler OnHapusData;
-      event EventHandler OnSimpanData;
-      event EventHandler OnBersihkanData;
-      event EventHandler<CurrentCellKeyEventArgs> OnListDataGridCurrentCellKeyDown;
-      event EventHandler<CurrentCellActivatedEventArgs> OnListDataGridCurrentCellActivated;
-      event EventHandler<CurrentCellEndEditEventArgs> OnListDataGridCurrentCellEndEdit;
-      event EventHandler<PreviewKeyDownEventArgs> OnListDataGridPreviewKeyDown;
-
-      ListDataGrid ListDataGrid { get; }
       ComboBoxSupplier ComboBoxSupplier { get; }
       Label LabelTotalQty { get; }
       Label LabelTotalPembelian { get; }
