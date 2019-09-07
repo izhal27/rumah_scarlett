e9492f778d729668e43f1741841b12b9603044d4
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-07 21:22:06 +0800

    add common controls panel info digital

---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/CommonControls/PanelInfoDigital.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RumahScarlett.Presentation.Views.CommonControls
+{
+   public partial class PanelInfoDigital : UserControl
+   {
+      public Label LabelInfo
+      {
+         get { return labelInfo; }
+      }
+
+      public PanelInfoDigital()
+      {
+         InitializeComponent();
+      }
+   }
+}
