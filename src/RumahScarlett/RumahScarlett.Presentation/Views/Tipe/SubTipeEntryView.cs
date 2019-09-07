6abc886b4e09c8b439e2decf0194a451a95aa7ee
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-07 22:53:46 +0800

    refactor sub tipe entry view

--- src/RumahScarlett/RumahScarlett.Presentation/Views/Tipe/SubTipeEntryView.cs
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/Tipe/SubTipeEntryView.cs
@@ -26,7 +26,7 @@ namespace RumahScarlett.Presentation.Views.Tipe
          InitializeComponent();
 
          _isNewData = isNewData;
-         panelUp.LabelInfo = isNewData ? "TAMBAH SUB TIPE" : "UBAH SUB TIPE";
+         panelUp.LabelInfo = isNewData ? $"TAMBAH {_typeName.ToUpper()}" : $"UBAH {_typeName.ToUpper()}";
 
          if (!_isNewData)
          {
