6ff6341f61e748cf9ac3acf025bb5cccedf30901
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-08 11:10:25 +0800

    add laporang penjualan view

---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/ILaporanView.cs
@@ -0,0 +1,21 @@
+﻿using RumahScarlett.Presentation.Views.CommonControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Presentation.Views
+{
+   public interface ILaporanView : IView
+   {
+      event EventHandler OnLoadData;
+      event EventHandler OnDeleteData;
+      event EventHandler OnPrintData;
+      event EventHandler OnDetailClick;
+      event EventHandler OnTampilkanClick;
+
+      DateTimePickerFilterTransaksi DateTimePickerFilterTransaksi { get; }
+      ListDataGrid ListDataGrid { get; }
+   }
+}
---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/Penjualan/LaporanPenjualanView.cs
@@ -0,0 +1,39 @@
+﻿using RumahScarlett.Presentation.Views.CommonControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RumahScarlett.Presentation.Views.Penjualan
+{
+   public partial class LaporanPenjualanView : BaseDataView, ILaporanView
+   {
+      public event EventHandler OnLoadData;
+      public event EventHandler OnDeleteData;
+      public event EventHandler OnPrintData;
+      public event EventHandler OnDetailClick;
+      public event EventHandler OnTampilkanClick;
+
+      public DateTimePickerFilterTransaksi DateTimePickerFilterTransaksi
+      {
+         get { return dateTimePickerFilterTransaksi; }
+      }
+
+      public ListDataGrid ListDataGrid
+      {
+         get { return listDataGrid; }
+      }
+
+      public LaporanPenjualanView()
+      {
+         InitializeComponent();
+
+         panelUp.LabelInfo = $"LAPORAN {Text.ToUpper()}";
+      }
+   }
+}
