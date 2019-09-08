5f672f6089034ae4646436790dc9749a4c0e66ae
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-08 09:40:02 +0800

    refactor pelanggan view

--- src/RumahScarlett/RumahScarlett.Presentation/Views/Pelanggan/PelangganView.cs
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/Pelanggan/PelangganView.cs
@@ -32,7 +32,7 @@ namespace RumahScarlett.Presentation.Views.Pelanggan
       {
          InitializeComponent();
 
-         panelUp.LabelInfo = "DATA PELANGGAN";
+         panelUp.LabelInfo = $"DATA {Text.ToUpper()}";
          crudcButtons.ButtonCetakVisible = false;
 
          listDataGrid.CellDoubleClick += ListDataGrid_CellDoubleClick;
