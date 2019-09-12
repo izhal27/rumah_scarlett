79e92d9963e64be0ee19db712d8aedab5c2c556d
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-12 11:18:33 +0800

    add laba rugi model

---
+++ src/RumahScarlett/RumahScarlett.Domain/Models/Laporan/ILabaRugiModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RumahScarlett.Domain.Models.Laporan
+{
+   public interface ILabaRugiModel
+   {
+      decimal total_penjualan { get; set; }
+      decimal total_hpp { get; set; }
+      decimal total_pengeluaran { get; set; }
+      decimal total_diskon_penjualan { get; set; }
+   }
+}
---
+++ src/RumahScarlett/RumahScarlett.Domain/Models/Laporan/LabaRugiModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Domain.Models.Laporan
+{
+   public class LabaRugiModel : ILabaRugiModel
+   {
+      public decimal total_penjualan { get; set; }
+      public decimal total_hpp { get; set; }
+      public decimal total_pengeluaran { get; set; }
+      public decimal total_diskon_penjualan { get; set; }
+   }
+}
