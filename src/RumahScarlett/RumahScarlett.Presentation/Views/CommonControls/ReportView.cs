dfa8e2b446a980e90df5db407807671917dec35c
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-12 22:11:26 +0800

    add report view

---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/CommonControls/ReportView.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RumahScarlett.Presentation.Views.CommonControls
+{
+   public partial class ReportView : Form
+   {
+
+      #region >> Fields <<
+
+      private string _reportNameSpace = @"RumahScarlett.Report.{0}.rdlc";
+      private Assembly _assemblyReport;
+
+      #endregion
+
+      // ----------------------------------------------------------------------//
+
+      #region >> Constructor <<
+         
+      public ReportView(string textForm, string reportName, ReportDataSource reportDataSource
+         , IEnumerable<ReportParameter> parameters = null)
+      {
+         InitializeComponent();
+
+         reportViewer1.SetDisplayMode(DisplayMode.PrintLayout);
+         reportViewer1.ZoomMode = ZoomMode.Percent;
+         reportViewer1.ZoomPercent = 100;
+
+         _assemblyReport = Assembly.LoadFrom("RumahScarlett.Report.dll");
+
+         Text = textForm; // Title Form preview
+
+         reportName = string.Format(_reportNameSpace, reportName);
+         var reportDefinition = _assemblyReport.GetManifestResourceStream(reportName);
+
+         reportViewer1.LocalReport.DataSources.Clear();
+
+         if (reportDataSource != null)
+            reportViewer1.LocalReport.DataSources.Add(reportDataSource);
+
+         reportViewer1.LocalReport.LoadReportDefinition(reportDefinition);
+
+         if (parameters != null)
+            reportViewer1.LocalReport.SetParameters(parameters);
+
+         reportViewer1.RefreshReport();
+      }
+
+      #endregion
+
+   }
+}
---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/CommonControls/ReportView.designer.cs
@@ -0,0 +1,64 @@
+﻿namespace RumahScarlett.Presentation.Views.CommonControls
+{
+   partial class ReportView
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportView));
+         this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+         this.SuspendLayout();
+         // 
+         // reportViewer1
+         // 
+         this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+         this.reportViewer1.Name = "reportViewer1";
+         this.reportViewer1.Size = new System.Drawing.Size(684, 462);
+         this.reportViewer1.TabIndex = 0;
+         // 
+         // ReportView
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(684, 462);
+         this.Controls.Add(this.reportViewer1);
+         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+         this.Name = "ReportView";
+         this.ShowInTaskbar = false;
+         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+         this.Tag = "ignore";
+         this.Text = "ReportPreview";
+         this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+   }
+}
