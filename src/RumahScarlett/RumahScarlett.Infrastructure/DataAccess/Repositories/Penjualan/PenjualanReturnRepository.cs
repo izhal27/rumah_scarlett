0ab339baf5e58e40442c855bf83ac119eb428143
Risal Walangadi <risal.walangadi@gmail.com>
2019-09-25 11:08:21 +0800

    add penjualan services and repository

---
+++ src/RumahScarlett/RumahScarlett.Infrastructure/DataAccess/Repositories/Penjualan/PenjualanReturnRepository.cs
@@ -0,0 +1,53 @@
+﻿using RumahScarlett.Domain.Models.Penjualan;
+using RumahScarlett.Services.Services.Penjualan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Infrastructure.DataAccess.Repositories.Penjualan
+{
+   public class PenjualanReturnRepository : BaseRepository<IPenjualanReturnModel>, IPenjualanReturnRepository
+   {
+      public PenjualanReturnRepository()
+      {
+         _modelName = "return penjualan";
+      }
+      
+      public void Insert(IPenjualanReturnModel model)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Update(IPenjualanReturnModel model)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Delete(IPenjualanReturnModel model)
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<IPenjualanReturnModel> GetAll()
+      {
+         throw new NotImplementedException();
+      }
+
+      public IPenjualanReturnModel GetById(object id)
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<IPenjualanReturnModel> GetByDate(object date)
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<IPenjualanReturnModel> GetByDate(object startDate, object endDate)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
---
+++ src/RumahScarlett/RumahScarlett.Services/Services/Penjualan/PenjualanReturnServices.cs
@@ -0,0 +1,62 @@
+﻿using RumahScarlett.Domain.Models.Penjualan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Services.Services.Penjualan
+{
+   public class PenjualanReturnServices : IPenjualanReturnServices
+   {
+      private IPenjualanReturnRepository _repo;
+      private IModelDataAnnotationCheck _modelDAC;
+
+      public PenjualanReturnServices(IPenjualanReturnRepository repo, IModelDataAnnotationCheck modelDAC)
+      {
+         _repo = repo;
+         _modelDAC = modelDAC;
+      }
+
+      public void Insert(IPenjualanReturnModel model)
+      {
+         ValidateModel(model);
+         _repo.Insert(model);
+      }
+
+      public void Update(IPenjualanReturnModel model)
+      {
+         throw new NotImplementedException();
+      }
+      
+      public void Delete(IPenjualanReturnModel model)
+      {
+         _repo.Delete(model);
+      }
+
+      public IEnumerable<IPenjualanReturnModel> GetAll()
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<IPenjualanReturnModel> GetByDate(object date)
+      {
+         return _repo.GetByDate(date);
+      }
+
+      public IEnumerable<IPenjualanReturnModel> GetByDate(object startDate, object endDate)
+      {
+         return _repo.GetByDate(startDate, endDate);
+      }
+
+      public IPenjualanReturnModel GetById(object id)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void ValidateModel(IPenjualanReturnModel model)
+      {
+         _modelDAC.ValidateModel(model);
+      }
+   }
+}
