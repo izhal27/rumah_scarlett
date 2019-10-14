719dd2b64fff81833bb549551ab0c01744ec4b20
Risal Walangadi <risal.walangadi@gmail.com>
2019-10-14 14:44:23 +0800

    add: user view

---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/Role/IRoleView.cs
@@ -0,0 +1,20 @@
+﻿using RumahScarlett.Presentation.Views.CommonControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RumahScarlett.Presentation.Views.Role
+{
+   public interface IRoleView : IDataView
+   {
+      event EventHandler OnButtonUpdateActionClick;
+      ComboBox ComboBoxMenu { get; }
+      TreeView TreeViewAction { get; }
+   }
+}
