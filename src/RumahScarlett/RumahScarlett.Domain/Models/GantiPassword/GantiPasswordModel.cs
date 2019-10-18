145fc9d48aeab449bda75c36216ca046dfbcb563
Risal Walangadi <risal.walangadi@gmail.com>
2019-10-18 14:31:24 +0800

    add: add ganti password model

---
+++ src/RumahScarlett/RumahScarlett.Domain/Models/GantiPassword/GantiPasswordModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Domain.Models.GantiPassword
+{
+   public class GantiPasswordModel : IGantiPasswordModel
+   {
+      public string login_id
+      {
+         get; set;
+      }
+
+      public string password_baru
+      {
+         get; set;
+      }
+
+      public string password_sekarang
+      {
+         get; set;
+      }
+
+      public string konf_password_baru
+      {
+         get; set;
+      }
+   }
+}
---
+++ src/RumahScarlett/RumahScarlett.Domain/Models/GantiPassword/IGantiPasswordModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RumahScarlett.Domain.Models.GantiPassword
+{
+   public interface IGantiPasswordModel
+   {
+      string login_id { get; set; }
+      string password_sekarang { get; set; }
+      string password_baru { get; set; }
+      string konf_password_baru { get; set; }
+   }
+}
