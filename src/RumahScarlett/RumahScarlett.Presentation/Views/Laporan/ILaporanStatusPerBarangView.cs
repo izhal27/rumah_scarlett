e373849e3be9159f8c14090a54cf7a061e6608ca
Risal Walangadi <risal.walangadi@gmail.com>
2019-10-19 14:01:53 +0800

    add: add laporan status per barang view

---
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/Laporan/ILaporanStatusPerBarangView.cs
@@ -0,0 +1,29 @@
+﻿using RumahScarlett.Presentation.Views.CommonControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RumahScarlett.Presentation.Views.Laporan
+{
+   public interface ILaporanStatusPerBarangView : IView
+   {
+      event EventHandler OnLoadView;
+      event EventHandler OnButtonTampilkanClick;
+      event EventHandler OnButtonCetakClick;
+
+      ListDataGrid ListDataGrid { get; }
+      RadioButton RadioButtonBulan { get; }
+      ComboBoxBulan ComboBoxBulan { get; }
+      NumericUpDown NumericUpDownTahun { get; }
+      ComboBoxBulan ComboBoxBulanAwal { get; }
+      NumericUpDown NumericUpDownTahunAwal { get; }
+      ComboBoxBulan ComboBoxBulanAkhir { get; }
+      NumericUpDown NumericUpDownTahunAkhir { get; }
+   }
+}
