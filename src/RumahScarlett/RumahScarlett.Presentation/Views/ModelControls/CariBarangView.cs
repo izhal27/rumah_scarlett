78be3a57d49466d51a7b31539205145cfd3db27d
Risal Walangadi <risal.walangadi@gmail.com>
2020-04-05 01:41:51 +0800

    add style and prevent send data when stok barang 0 at transaksi penjualan

--- src/RumahScarlett/RumahScarlett.Presentation/Views/ModelControls/CariBarangView.cs
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/ModelControls/CariBarangView.cs
@@ -2,6 +2,8 @@
 using RumahScarlett.CommonComponents;
 using RumahScarlett.Domain.Models.Barang;
 using RumahScarlett.Presentation.Views.CommonControls;
+using Syncfusion.WinForms.DataGrid.Enums;
+using Syncfusion.WinForms.DataGrid.Events;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,93 +16,132 @@ using System.Windows.Forms;
 
 namespace RumahScarlett.Presentation.Views.ModelControls
 {
-   public partial class CariBarangView : BaseCariBarangView
-   {
-      public event EventHandler OnSendData;
+  public partial class CariBarangView : BaseCariBarangView
+  {
+    public event EventHandler OnSendData;
 
-      private List<IBarangModel> _listsBarang;
-      private BindingListView<BarangModel> _bindingView;
-      private string _kodeOrNamaValue;
+    private List<IBarangModel> _listsBarang;
+    private TipePencarian _tipePencarian;
+    private BindingListView<BarangModel> _bindingView;
+    private string _kodeOrNamaValue;
 
-      public CariBarangView()
+    public CariBarangView()
+    {
+      InitializeComponent();
+    }
+
+    public CariBarangView(List<IBarangModel> listsBarang, TipePencarian tipePencarian, string kodeOrNamaValue = default(string)) : this()
+    {
+      _listsBarang = listsBarang;
+      _tipePencarian = tipePencarian;
+      _bindingView = new BindingListView<BarangModel>(_listsBarang);
+      listDataGrid.DataSource = _bindingView;
+
+      listDataGrid.Columns[2].Visible = false; // Stok
+      listDataGrid.Columns[7].Visible = false; // Penyesuaian stok
+
+      switch (tipePencarian)
       {
-         InitializeComponent();
+        case TipePencarian.Penjualan:
+
+          listDataGrid.Columns[4].Visible = false; // Hpp
+          listDataGrid.Columns[6].Visible = false; // Minimal stok
+
+          break;
+        case TipePencarian.Pembelian:
+
+          listDataGrid.Columns[5].Visible = false; // Harga jual
+          listDataGrid.Columns[6].Visible = false; // Minimal stok
+
+          break;
+        case TipePencarian.PenyesuaianStok:
+
+          listDataGrid.Columns[5].Visible = false; // Harga jual
+
+          break;
       }
 
-      public CariBarangView(List<IBarangModel> listsBarang, TipePencarian tipePencarian, string kodeOrNamaValue = default(string)) : this()
+      _kodeOrNamaValue = kodeOrNamaValue;
+
+      if (tipePencarian == TipePencarian.Penjualan)
       {
-         _listsBarang = listsBarang;
-         _bindingView = new BindingListView<BarangModel>(_listsBarang);
-         listDataGrid.DataSource = _bindingView;
-
-         listDataGrid.Columns[7].Visible = false; // Penyesuaian stok
-
-         switch (tipePencarian)
-         {
-            case TipePencarian.Penjualan:
-
-               listDataGrid.Columns[4].Visible = false; // Hpp
-               listDataGrid.Columns[6].Visible = false; // Minimal stok
-
-               break;
-            case TipePencarian.Pembelian:
-
-               listDataGrid.Columns[5].Visible = false; // Harga jual
-               listDataGrid.Columns[6].Visible = false; // Minimal stok
-
-               break;
-            case TipePencarian.PenyesuaianStok:
-
-               listDataGrid.Columns[5].Visible = false; // Harga jual
-
-               break;
-         }
-
-         _kodeOrNamaValue = kodeOrNamaValue;
-
-         OnEnterKeyDown += CariBarangPembelianView_OnEnterKeyDown;
+        listDataGrid.QueryRowStyle += ListDataGrid_QueryRowStyle;
       }
 
-      private void CariBarangView_Load(object sender, EventArgs e)
+      OnEnterKeyDown += CariBarangPembelianView_OnEnterKeyDown;
+    }
+
+    private void CariBarangView_Load(object sender, EventArgs e)
+    {
+      listDataGrid.AutoSizeColumnsMode = AutoSizeColumnsMode.LastColumnFill;
+      listDataGrid.Columns["nama"].AutoSizeColumnsMode = AutoSizeColumnsMode.LastColumnFill;
+
+      if (!string.IsNullOrWhiteSpace(_kodeOrNamaValue))
       {
-         if (!string.IsNullOrWhiteSpace(_kodeOrNamaValue))
-         {
-            textBoxPencarian.Text = _kodeOrNamaValue;
-         }
+        textBoxPencarian.Text = _kodeOrNamaValue;
       }
+    }
 
-      private void CariBarangPembelianView_OnEnterKeyDown(object sender, EventArgs e)
+    private void ListDataGrid_QueryRowStyle(object sender, QueryRowStyleEventArgs e)
+    {
+      if (e.RowType == RowType.DefaultRow)
       {
-         if (listDataGrid.SelectedItem != null)
-         {
-            var model = (BarangModel)listDataGrid.SelectedItem;
+        var barangModel = (BarangModel)e.RowData;
+        var oldStyle = e.Style;
 
-            OnSendData?.Invoke(this, new ModelEventArgs<BarangModel>((BarangModel)listDataGrid.SelectedItem));
-         }
+        if (barangModel != null)
+        {
+          if (barangModel.stok <= 0)
+          {
+            e.Style.BackColor = Color.FromArgb(255, 165, 160);
+            e.Style.TextColor = Color.FromArgb(1, 2, 3);
+          }
+          else
+          {
+            e.Style.BackColor = oldStyle.BackColor;
+            e.Style.TextColor = oldStyle.TextColor;
+          }
+        }
       }
+    }
 
-      protected override void textBoxPencarian_TextChanged(object sender, EventArgs e)
+    private void CariBarangPembelianView_OnEnterKeyDown(object sender, EventArgs e)
+    {
+      if (listDataGrid.SelectedItem != null)
       {
-         var textBox = (TextBox)sender;
+        var model = (BarangModel)listDataGrid.SelectedItem;
 
-         if (!string.IsNullOrWhiteSpace(textBox.Text))
-         {
-            var value = textBox.Text;
+        if (_tipePencarian == TipePencarian.Penjualan && model.stok <= 0)
+        {
+          return;
+        }
 
-            _bindingView.DataSource = _listsBarang.Where(b => b.kode.ToLower().Contains(value.ToLower()) ||
-                                                         b.nama.ToLower().Contains(value.ToLower())).ToList();
-         }
-         else
-         {
-            _bindingView.DataSource = _listsBarang;
-         }
+        OnSendData?.Invoke(this, new ModelEventArgs<BarangModel>((BarangModel)listDataGrid.SelectedItem));
       }
-   }
+    }
 
-   public enum TipePencarian
-   {
-      Pembelian,
-      Penjualan,
-      PenyesuaianStok,
-   }
+    protected override void textBoxPencarian_TextChanged(object sender, EventArgs e)
+    {
+      var textBox = (TextBox)sender;
+
+      if (!string.IsNullOrWhiteSpace(textBox.Text))
+      {
+        var value = textBox.Text;
+
+        _bindingView.DataSource = _listsBarang.Where(b => b.kode.ToLower().Contains(value.ToLower()) ||
+                                                     b.nama.ToLower().Contains(value.ToLower())).ToList();
+      }
+      else
+      {
+        _bindingView.DataSource = _listsBarang;
+      }
+    }
+  }
+
+  public enum TipePencarian
+  {
+    Pembelian,
+    Penjualan,
+    PenyesuaianStok,
+  }
 }
