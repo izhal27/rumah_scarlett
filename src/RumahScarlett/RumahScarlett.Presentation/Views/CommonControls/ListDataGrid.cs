b73cca03594e99245968f53bc374644844d2adc0
Risal Walangadi <risal.walangadi@gmail.com>
2020-04-07 09:36:38 +0800

    set allow resizing column at list data grid

--- src/RumahScarlett/RumahScarlett.Presentation/Views/CommonControls/ListDataGrid.cs
+++ src/RumahScarlett/RumahScarlett.Presentation/Views/CommonControls/ListDataGrid.cs
@@ -12,40 +12,47 @@ using System.Windows.Forms.VisualStyles;
 
 namespace RumahScarlett.Presentation.Views.CommonControls
 {
-   public class ListDataGrid : SfDataGrid
-   {
-      public ListDataGrid()
-      {
-         ShowRowHeader = true;
-         AllowEditing = false;
-         AutoSizeColumnsMode = AutoSizeColumnsMode.Fill;
-         Anchor = AnchorStyles.Top | AnchorStyles.Right |
-                  AnchorStyles.Bottom | AnchorStyles.Left;
+  public class ListDataGrid : SfDataGrid
+  {
+    public ListDataGrid()
+    {
+      AutoSizeColumnsMode = AutoSizeColumnsMode.Fill;
+      Anchor = AnchorStyles.Top | AnchorStyles.Right |
+               AnchorStyles.Bottom | AnchorStyles.Left;
 
-         DrawCell += ListDataGrid_DrawCell;
-         QueryRowStyle += ListDataGrid_QueryRowStyle;
-      }
+      DrawCell += ListDataGrid_DrawCell;
+      QueryRowStyle += ListDataGrid_QueryRowStyle;
+      
+    }
 
-      private void ListDataGrid_DrawCell(object sender, DrawCellEventArgs e)
-      {
-         if (ShowRowHeader && e != null && e.RowIndex != 0)
-         {
-            if (e.ColumnIndex == 0)
-            {
-               e.DisplayText = e.RowIndex.ToString();
-               e.Style.TextColor = Color.Black;
-               e.Style.VerticalAlignment = VerticalAlignment.Center;
-               e.Style.HorizontalAlignment = HorizontalAlignment.Center;
-            }
-         }
-      }
+    protected override void OnCreateControl()
+    {
+      base.OnCreateControl();
+      ShowRowHeader = true;
+      AllowEditing = false;
+      AllowResizingColumns = true;
+    }
 
-      private void ListDataGrid_QueryRowStyle(object sender, QueryRowStyleEventArgs e)
+    private void ListDataGrid_DrawCell(object sender, DrawCellEventArgs e)
+    {
+      if (ShowRowHeader && e != null && e.RowIndex != 0)
       {
-         if (e.RowType == RowType.DefaultRow && e.RowIndex % 2 == 0)
-            e.Style.BackColor = MainProgram.Pengaturan.warna_baris_genap;
-         else
-            e.Style.BackColor = MainProgram.Pengaturan.warna_baris_ganjil;
+        if (e.ColumnIndex == 0)
+        {
+          e.DisplayText = e.RowIndex.ToString();
+          e.Style.TextColor = Color.Black;
+          e.Style.VerticalAlignment = VerticalAlignment.Center;
+          e.Style.HorizontalAlignment = HorizontalAlignment.Center;
+        }
       }
-   }
+    }
+
+    private void ListDataGrid_QueryRowStyle(object sender, QueryRowStyleEventArgs e)
+    {
+      if (e.RowType == RowType.DefaultRow && e.RowIndex % 2 == 0)
+        e.Style.BackColor = MainProgram.Pengaturan.warna_baris_genap;
+      else
+        e.Style.BackColor = MainProgram.Pengaturan.warna_baris_ganjil;
+    }
+  }
 }
